caa038f4bf02b12e6c24bc909ef594aa8d48d396
Gia Bao <thainguyengiabao27092004@gmail.com>
2024-08-25 21:56:34 +0700

    s

--- index.mjs
+++ index.mjs
@@ -1,13 +1,13 @@
 // const core = require('@actions/core');
-import core from '@actions/core';
-import { getData } from './getData.cjs';
-import { drawChart } from './drawChart.mjs';
+import core from "@actions/core";
+import { getData } from "./getData.mjs";
+import { drawChart } from "./drawChart.mjs";
 
 async function run() {
   try {
-    const orgName = core.getInput('org-name');
+    const orgName = core.getInput("org-name");
     console.log(`Input received: ${orgName}`);
-    const token = core.getInput('token');
+    const token = core.getInput("token");
     // Get data
     const data = await getData(orgName, token);
     console.log(data);
@@ -15,7 +15,7 @@ async function run() {
     await drawChart(data);
 
     // Set the output
-    core.setOutput('myOutput', "Hello World");
+    core.setOutput("myOutput", "Hello World");
   } catch (error) {
     core.setFailed(error.message);
   }
